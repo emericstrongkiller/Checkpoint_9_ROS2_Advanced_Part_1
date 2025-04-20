d768dbec51cf84911329034dcbc15edffa9055aa
emericstrongkiller <arhant.emeric-is@outlook.fr>
2025-04-20 15:07:17 +0000

    added custom interface package for GoToLanding.srv interface, modified simple approach service to work with GoToLanding interface

--- attach_shelf/src/approach_service_server.cpp
+++ attach_shelf/src/approach_service_server.cpp
@@ -1,6 +1,6 @@
+#include "custom_interfaces/srv/go_to_loading.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/laser_scan.hpp"
-#include "std_srvs/srv/trigger.hpp"
 #include <string>
 
 class FinalApproach : public rclcpp::Node {
@@ -8,20 +8,20 @@ public:
   FinalApproach(const std::string &approach_service_name)
       : Node("approach_service") {
     // Service definition
-    approach_shelf_ = this->create_service<std_srvs::srv::Trigger>(
+    approach_shelf_ = this->create_service<custom_interfaces::srv::GoToLoading>(
         approach_service_name,
         std::bind(&FinalApproach::handle_approach_request, this,
                   std::placeholders::_1, std::placeholders::_2));
   }
 
 private:
-  rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr approach_shelf_;
+  rclcpp::Service<custom_interfaces::srv::GoToLoading>::SharedPtr
+      approach_shelf_;
 
   void handle_approach_request(
-      const std_srvs::srv::Trigger::Request::SharedPtr request,
-      std_srvs::srv::Trigger::Response::SharedPtr response) {
-    response->success = true;
-    response->message = "message_recieved !";
+      const custom_interfaces::srv::GoToLoading::Request::SharedPtr request,
+      custom_interfaces::srv::GoToLoading::Response::SharedPtr response) {
+    response->complete = true;
     RCLCPP_INFO(this->get_logger(), "Recieved message from service !");
   }
 };
